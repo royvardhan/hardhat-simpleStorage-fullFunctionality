7a9c1ba6b3df7a63d94b58250e6d7499da189037
royvardhan <royvardhan.official@gmail.com>
2022-08-05 18:47:25 +0530

    added localhost in hardhat.config.js file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
+require("./tasks/block-number");
 
 const privateKey = process.env.PRIVATE_KEY;
 const goerli = process.env.GOERLI_RPC_URL;
 const ETHERSCAN = process.env.ETHERSCAN_API_KEY;
+const hhprivateKey = process.env.HARDHAT_PRIVATE_KEY;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
@@ -13,6 +15,11 @@ module.exports = {
       url: goerli,
       accounts: [privateKey],
       chainId: 5,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      accounts: [hhprivateKey],
+      chainId: 31337,
     },
   },
   etherscan: {
